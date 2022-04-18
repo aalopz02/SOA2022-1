fadc32046c252527c6fc6700ef735f99c8bc0adb
Andres Artavia <aalopz02@gmail.com>
2022-04-17 23:56:46 -0600

    no

--- Proyecto1/AndresA/Project/terraform/bucket.tf
+++ Proyecto1/AndresA/Project/terraform/bucket.tf
@@ -3,20 +3,6 @@ resource "google_storage_bucket" "function_bucket" {
   location = var.region
 }
 
-data "google_iam_policy" "writer" {
-  binding {
-    role = "roles/storage.objectAdmin"
-    members = [
-        "allUsers",
-    ] 
-  }
-}
-
-resource "google_storage_bucket_iam_policy" "editor" {
-  bucket = "${google_storage_bucket.input_bucket.name}"
-  policy_data = "${data.google_iam_policy.writer.policy_data}"
-}
-
 resource "google_storage_bucket" "input_bucket" {
   name = "${var.project_id}-input"
   location = var.region
